c5b56e6277138b5a60fb47552da3a6a00163b8d7
Sergey Bukharov <s.buharov@dodopizza.com>
2018-02-16 15:40:33 +0300

    Clonning git repository

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require('express')
 const exec = require('child_process').exec
+const fs = require('fs')
 
 const app = express()
 
-app.get('/refresh-container', (request, response) => {
-    exec('ls', (err, stdout, stderr) => {
-        response.json({ stdout: stdout })
+
+const botConstructorRepositoryLink = 'https://github.com/s-buhar0v/telegram-bot-constructor-bot'
+
+app.get('/refresh-image', (request, response) => {
+    fs.exists('./temp/telegram-bot-constructor', botConstructorExists => {
+        if (botConstructorExists) {
+            exec(`cd ./temp/telegram-bot-constructor; git clone ${botConstructorRepositoryLink}`, (err, stdout, stderr) => {
+                response.json({ stdout: stdout })
+            })
+        } else {
+            exec(`cd ./temp/telegram-bot-constructor; git pull`, (err, stdout, stderr) => {
+                response.json({ stdout: stdout })
+            })
+        }
     })
 })
 
