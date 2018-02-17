cfbac64b0517d13f0a12ce077dfff88ace012f0a
Sergey Bukharov <s.buharov@dodopizza.com>
2018-02-17 16:31:59 +0300

    Debug print

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.get('/refresh-image', (request, response) => {
     exec('chmod +x ./scritps/refresh-image.sh', (err, stdout, stderr) => {
-        console.log(err, stderr)
+        console.log(process.env.HOME)
         exec('./scritps/refresh-image.sh', (err, stdout, stderr) => {
             console.log(err, stderr)
             response.json({ stdout: stdout })
