40a48ca9e41133a2ab07b847a1a8d0c3abccecf6
Sergey Bukharov <s.buharov@dodopizza.com>
2018-02-17 17:04:33 +0300

    try to change paths

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const exec = require('child_process').exec
-const path =  require('path')
+const path = require('path')
 
 const app = express()
 
@@ -9,20 +9,16 @@ const refreshImageScriptPath = path.join(process.env.HOME, 'telegram-bot-constru
 console.log(`sudo chmod +x ${refreshImageScriptPath}`)
 
 app.get('/refresh-image', (request, response) => {
-    exec(`if ! test -d $HOME/telegram-bot-constructor/telegram-bot-constructor-run-server/temp/telegram-bot-constructor-bot
+    exec(`if ! test -d ./temp/telegram-bot-constructor-bot
           then
-            cd $HOME/telegram-bot-constructor/telegram-bot-constructor-run-server/temp/
+            cd ./temp/
             git clone https://github.com/s-buhar0v/telegram-bot-constructor-bot.git
-        else
-            cd $HOME/telegram-bot-constructor/telegram-bot-constructor-run-server/temp/telegram-bot-constructor-bot
-            git pull
-        fi`, (err, stdout, stderr) => {
-        console.log(stdout, stderr)
-        exec(`${refreshImageScriptPath}`, (err, stdout, stderr) => {
-            console.log(err, stderr)
-            response.json({ stdout: stdout })
+            else
+                cd ./temp/telegram-bot-constructor-bot
+                git pull
+            fi`, (err, stdout, stderr) => {
+            console.log(stdout, stderr)
         })
-    })
 })
 
 app.listen(3000)
