99daaca8f7b0e2a8c743e5082dfb5c25659bb937
Sergey Bukharov <s.buharov@dodopizza.com>
2018-02-17 17:01:07 +0300

    try to run string with script

--- server.js
+++ server.js
@@ -9,7 +9,14 @@ const refreshImageScriptPath = path.join(process.env.HOME, 'telegram-bot-constru
 console.log(`sudo chmod +x ${refreshImageScriptPath}`)
 
 app.get('/refresh-image', (request, response) => {
-    exec(`sudo chmod +x ${refreshImageScriptPath}`, (err, stdout, stderr) => {
+    exec(`if ! test -d $HOME/telegram-bot-constructor/telegram-bot-constructor-run-server/temp/telegram-bot-constructor-bot
+          then
+            cd $HOME/telegram-bot-constructor/telegram-bot-constructor-run-server/temp/
+            git clone https://github.com/s-buhar0v/telegram-bot-constructor-bot.git
+        else
+            cd $HOME/telegram-bot-constructor/telegram-bot-constructor-run-server/temp/telegram-bot-constructor-bot
+            git pull
+        fi`, (err, stdout, stderr) => {
         console.log(stdout, stderr)
         exec(`${refreshImageScriptPath}`, (err, stdout, stderr) => {
             console.log(err, stderr)
