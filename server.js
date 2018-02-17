236fb7f49507be7c6fb761d1616e3b977361807d
Sergey Bukharov <s.buharov@dodopizza.com>
2018-02-17 16:36:59 +0300

    Full path to scriot

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const exec = require('child_process').exec
-const fs = require('fs')
+const path =  require('path')
 
 const app = express()
 
+const refreshImageScriptPath = path.join(process.env.HOME, 'telegram-bot-constructor/telegram-bot-constructor-run-server/scritps/refresh-image.sh')
+
 app.get('/refresh-image', (request, response) => {
-    exec('chmod +x ./scritps/refresh-image.sh', (err, stdout, stderr) => {
-        console.log(process.env.HOME)
-        exec('./scritps/refresh-image.sh', (err, stdout, stderr) => {
+    exec(`chmod +x ${refreshImageScriptPath}`, (err, stdout, stderr) => {
+        console.log(stdout, stderr)
+        exec(`${refreshImageScriptPath}`, (err, stdout, stderr) => {
             console.log(err, stderr)
             response.json({ stdout: stdout })
         })
