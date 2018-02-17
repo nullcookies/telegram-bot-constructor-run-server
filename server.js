d7ea219a360d3ded488f92de9fe94f65f027ce59
Sergey Bukharov <s.buharov@dodopizza.com>
2018-02-17 17:40:40 +0300

    pix

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ app.get('/refresh-image', (request, response) => {
         then
             cd ./temp
             git clone https://github.com/s-buhar0v/telegram-bot-constructor-bot.git
-            cd ./telegram-bot-constructor-bot
         else
             cd ./temp/telegram-bot-constructor-bot
             git pull
