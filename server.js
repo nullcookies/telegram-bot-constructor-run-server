e9b12df3ff1e86069e14b24abdf36bb5bbe25f91
Sergey Bukharov <s.buharov@dodopizza.com>
2018-03-14 23:53:39 +0300

    fix

--- server.js
+++ server.js
@@ -7,22 +7,39 @@ const scripts = require('./scritps')
 const app = express()
 
 app.get('/refresh-image', (request, response) => {
-    exec(scripts.cloneRepository, (err, stdout, stderr) => {
-        console.log(err)
-        console.log(stderr)
-        console.log(stdout)
-        exec(scripts.buildImage, (err, stdout, stderr) => {
+    exec(`
+        if ! test -d ./temp/telegram-bot-constructor-bot
+        then
+            cd ./temp
+            git clone https://github.com/s-buhar0v/telegram-bot-constructor-bot.git
+        else
+            cd ./temp/telegram-bot-constructor-bot
+            git pull
+        fi`, (err, stdout, stderr) => {
             console.log(err)
             console.log(stderr)
             console.log(stdout)
-            if (err || stderr) {
-                response.json({ response: 'Failed to buildimage' })
-            } else {
-                response.json({ response: 'Image has been built successfully' })
-            }
-        })
+            exec(`
+                cd ./temp/telegram-bot-constructor-bot
+                if sudo docker ps -a -q --filter="ancestor=dev-bot-1/bot" 
+                then
+                    sudo docker stop $(sudo docker ps -a -q --filter="ancestor=dev-bot-1/bot")
+                    sudo docker rm $(sudo docker ps -a -q --filter="ancestor=dev-bot-1/bot")
+                fi
 
-    })
+                sudo docker rmi dev-bot-1/bot
+                sudo docker build -t dev-bot-1/bot .`, (err, stdout, stderr) => {
+                    console.log(err)
+                    console.log(stderr)
+                    console.log(stdout)
+                    if (err || stderr) {
+                        response.json({ response: 'Failed to buildimage' })
+                    } else {
+                        response.json({ response: 'Image has been built successfully' })
+                    }
+                })
+
+        })
 })
 
 app.listen(3000)
