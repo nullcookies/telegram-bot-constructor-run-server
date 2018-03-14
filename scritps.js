35995097a5071d41a4044e2d2016649c7555d0dd
Sergey Bukharov <s.buharov@dodopizza.com>
2018-03-15 00:01:10 +0300

    cloning git repo

--- scritps.js
+++ scritps.js
@@ -3,10 +3,10 @@ module.exports = {
     if ! test -d ./temp/telegram-bot-constructor-bot
     then
         cd ./temp
-        git clone https://github.com/s-buhar0v/telegram-bot-constructor-bot.git
+        sudo git clone https://github.com/s-buhar0v/telegram-bot-constructor-bot.git
     else
         cd ./temp/telegram-bot-constructor-bot
-        git pull
+        sudo git pull
     fi`,
 
     buildImage: `
--- server.js
+++ server.js
@@ -7,27 +7,17 @@ const scripts = require('./scritps')
 const app = express()
 
 app.get('/refresh-image', (request, response) => {
-    exec(`
-        ls
-        if ! test -d ./temp/telegram-bot-constructor-bot
-        then
-            cd ./temp
-            sudo git clone https://github.com/s-buhar0v/telegram-bot-constructor-bot.git
-        else
-            cd ./temp/telegram-bot-constructor-bot
-            sudo git pull
-        fi`, (err, stdout, stderr) => {
+    exec(scripts.cloneRepository, (err, stdout, stderr) => {
+        console.log(err)
+        console.log(`stderr:${stderr})
+        console.log(stdout)
+        if (err || stderr) {
+            response.json({ response: 'Failed to buildimage' })
+        } else {
+            response.json({ response: 'Image has been built successfully' })
+        }
 
-            console.log(err)
-            console.log(stderr)
-            console.log(stdout)
-            if (err || stderr) {
-                response.json({ response: 'Failed to buildimage' })
-            } else {
-                response.json({ response: 'Image has been built successfully' })
-            }
-
-        })
+    })
 })
 
 app.listen(3000)
