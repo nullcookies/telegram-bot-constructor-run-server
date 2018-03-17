14adaab0ea0288a59204ab7d0c031fa44beaf9ce
Sergey Bukharov <s.buharov@dodopizza.com>
2018-03-17 17:57:40 +0300

    rebuilding image fix

--- commands.js
+++ commands.js
@@ -16,7 +16,6 @@ module.exports = {
     if [ "$(sudo docker images -q dev-bot-1/bot)" != "" ]; then
         if [ "$(sudo docker ps -a -q --filter="ancestor=dev-bot-1/bot")" != "" ]; then
             sudo docker stop $(sudo docker ps -a -q --filter="ancestor=dev-bot-1/bot")
-            sudo docker rm $(sudo docker ps -a -q --filter="ancestor=dev-bot-1/bot")
         fi  
     fi`,
     removeImage: `
